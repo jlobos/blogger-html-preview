b2515b1497b5293fb52086a4682e7d6ccc8f282e
Jesús Lobos <jlobos@outlook.cl>
2017-07-08 06:20:49 -0400

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const fetch = require('isomorphic-fetch')
+const qs = require('qs')
+
+module.exports = ({
+  url,
+  token,
+  templateText,
+  withWidgets = true
+}) => {
+  return fetch(
+    `${url}/b/html-preview?token=${token}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: qs.stringify({withWidgets, templateText})
+    }
+  ).then(res => res.text())
+}
